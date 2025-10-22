f7d162540a3861debbcb62edda3ab509700a07f3
abraham-mendez-debug <abraham.me001@gmail.com>
2025-10-22 14:24:50 -0400

    Added an overloaded format() method which accepts a prefix, firstName, middleName, lastName and, suffix, then formats it into a String output.

--- src/main/java/com/pluralsight/NameFormatter.java
+++ src/main/java/com/pluralsight/NameFormatter.java
@@ -22,5 +22,34 @@ public class NameFormatter {
     }
 
 
+    public static String format (String prefix,
+                                 String firstName,
+                                 String middleName,
+                                 String lastName,
+                                 String suffix) {
+
+        StringBuilder sb = new StringBuilder();
+
+        prefix = prefix.trim();
+        firstName = firstName.trim();
+        middleName = middleName.trim();
+        lastName = lastName.trim();
+        suffix = suffix.trim();
+
+        sb.append(lastName);
+        sb.append(", ");
+        sb.append(prefix);
+        sb.append(" ");
+        sb.append(firstName);
+        sb.append(" ");
+        sb.append(middleName);
+        sb.append(", ");
+        sb.append(suffix);
+
+        return sb.toString();
+
+    }
+
+
 
 }
