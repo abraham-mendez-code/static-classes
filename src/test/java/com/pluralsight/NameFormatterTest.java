d0474f304a49f0b288bc4c4a76f2284d221b1fc7
abraham-mendez-debug <abraham.me001@gmail.com>
2025-10-22 14:26:48 -0400

    Added the NameFormatterTest.format_returns_lastName_prefix_firstName_middleName_suffix() test for the overloaded format() method. This test ensures prefix, firstName, middleName, lastName and, suffix are formatted correctly in the output String.

--- src/test/java/com/pluralsight/NameFormatterTest.java
+++ src/test/java/com/pluralsight/NameFormatterTest.java
@@ -22,5 +22,23 @@ class NameFormatterTest {
 
     }
 
+    @Test
+    void format_returns_lastName_prefix_firstName_middleName_suffix() {
+
+        // arrange
+        String prefix = "Dr.";
+        String firstName = "Mel";
+        String middleName = "B";
+        String lastName = "Johnson";
+        String suffix = "PhD";
+        String expectedFormat = "Johnson, Dr. Mel B, PhD";
+
+        // act
+        String actualFormat = NameFormatter.format(prefix, firstName, middleName, lastName, suffix);
+
+        // assert
+        assertTrue(expectedFormat.equals(actualFormat));
+
+    }
 
 }
