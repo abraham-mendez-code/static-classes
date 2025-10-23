bcdb11dbdd8f311fa2e699c79946eb9d159331c6
abraham-mendez-debug <abraham.me001@gmail.com>
2025-10-23 13:11:36 -0400

    Added overloaded format() methods

--- src/main/java/com/pluralsight/NameFormatter.java
+++ src/main/java/com/pluralsight/NameFormatter.java
@@ -1,6 +1,8 @@
 package com.pluralsight;
 
+import java.security.AllPermission;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class NameFormatter {
 
@@ -21,35 +23,68 @@ public class NameFormatter {
         return sb.toString();
     }
 
-
-    public static String format (String prefix,
-                                 String firstName,
-                                 String middleName,
-                                 String lastName,
-                                 String suffix) {
-
+    //method to process individual name parts
+    public static String format(String prefix,
+                                String firstName,
+                                String middleName,
+                                String lastName,
+                                String suffix) {
         StringBuilder sb = new StringBuilder();
+        sb.append(lastName).append(", ");
 
-        prefix = prefix.trim();
-        firstName = firstName.trim();
-        middleName = middleName.trim();
-        lastName = lastName.trim();
-        suffix = suffix.trim();
+        if (!prefix.isEmpty()) {
+            sb.append(prefix).append(" ");
+        }
 
-        sb.append(lastName);
-        sb.append(", ");
-        sb.append(prefix);
-        sb.append(" ");
         sb.append(firstName);
-        sb.append(" ");
-        sb.append(middleName);
-        sb.append(", ");
-        sb.append(suffix);
+
+        if (!middleName.isEmpty()) {
+            sb.append(" ").append(middleName);
+        }
+
+        //first issue fixed
+        if(!suffix.isEmpty()){
+            sb.append(", ").append(suffix);
+        }
+
 
         return sb.toString();
-
     }
 
+    //method to format from a string
+    public static String format(String fullName) {
+        //split on the comma if there
+        String[] parts = fullName.split(",", 2);
+        //grab the first item in the list and remove spaces
+        String namePart = parts[0].trim();
+        //if the length of parts is greater that one we know we had a suffix because we split on the ,
+        String suffix = parts.length > 1 ? parts[1].trim() : "";
 
+        //split the rest of the name on the space
+        String[] nameTokens = namePart.split(" ");
+        String prefix = "";
+        String firstName = "";
+        String middleName = "";
+        String lastName = "";
+
+        //depending on the length of the array we know what name parts we have
+        if (nameTokens.length == 4) {
+            //fix 2
+            firstName = nameTokens[1];
+            prefix = nameTokens[0];
+            middleName = nameTokens[2];
+            lastName = nameTokens[3];
+        } else if (nameTokens.length == 3) {
+            firstName = nameTokens[0];
+            middleName = nameTokens[1];
+            lastName = nameTokens[2];
+        } else if (nameTokens.length == 2) {
+            firstName = nameTokens[0];
+            lastName = nameTokens[1];
+        }
+
+        //call the format function above with the parts
+        return format(prefix, firstName, middleName, lastName, suffix);
+    }
 
 }
